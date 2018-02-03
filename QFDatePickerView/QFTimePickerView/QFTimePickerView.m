ff846d61c688e0ca0e25d8ca3dee94f9ff195bc7
流年释残忆 <291753906@qq.com>
2018-02-03 21:06:33 +0800

    新增纯年份选择，修复月份数据不全bug

--- QFDatePickerView/QFTimePickerView/QFTimePickerView.m
+++ QFDatePickerView/QFTimePickerView/QFTimePickerView.m
@@ -30,6 +30,15 @@
 
 @implementation QFTimePickerView
 
+/**
+ 初始化方法
+ 
+ @param startHour 其实时间点 时
+ @param endHour 结束时间点 时
+ @param period 间隔多少分中
+ @param block 返回选中的时间
+ @return QFTimePickerView实例
+ */
 - (instancetype)initDatePackerWithStartHour:(NSString *)startHour endHour:(NSString *)endHour period:(NSInteger)period response:(void (^)(NSString *))block{
     if (self = [super init]) {
         self.frame = [UIScreen mainScreen].bounds;
--- QFDatePickerView/ViewController.m
+++ QFDatePickerView/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-#import "QFDatePickerView.h"
 
+#import "QFDatePickerView.h"
 #import "QFTimePickerView.h"
 
 @interface ViewController ()
@@ -21,6 +21,7 @@
     [super viewDidLoad];
     
 }
+
 - (IBAction)showYearSelectView:(id)sender {
     QFDatePickerView *datePickerView = [[QFDatePickerView alloc]initYearPickerViewWithResponse:^(NSString *str) {
         NSString *string = str;
@@ -36,22 +37,15 @@
         NSLog(@"str = %@",string);
     }];
     [datePickerView show];
-
-    
 }
+
 - (IBAction)showTimePickerVierw:(id)sender {
     
-    QFTimePickerView *pickerView = [[QFTimePickerView alloc]initDatePackerWithStartHour:@"18" endHour:@"12" period:10 response:^(NSString *str) {
+    QFTimePickerView *pickerView = [[QFTimePickerView alloc]initDatePackerWithStartHour:@"18" endHour:@"12" period:5 response:^(NSString *str) {
         NSString *string = str;
         NSLog(@"str = %@",string);
     }];
     [pickerView show];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
